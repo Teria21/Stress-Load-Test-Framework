8a63b5dceb650a376bb2a8e0008a6385153018c6
Teria21 <teriia123@outlook.com>
2024-08-10 18:04:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var morgan = require('morgan');
 
 app.use(morgan('dev'));
-app.use(express.static('./www'));
+app.use(express.static('public'));
 
 app.listen(3000,function(){
     console.log('Express start on port 3000 ');
